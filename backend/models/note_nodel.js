4bdb76e9c3a33bb5d0b76515059fab08a3831616
Parul Goyal <149240936+Parulgoyal6@users.noreply.github.com>
2024-10-24 12:01:08 +0530

    Update note_nodel.js

--- backend/models/note_nodel.js
+++ backend/models/note_nodel.js
@@ -2,25 +2,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  title:{type : String, required: true},
-
-  content:{type: String, required: true},
-
-  tags:{ type:[String], default: []},
-
-  isPinned: {type : Boolean, default: false},
-
-  userId: {type : String, required: true},
-
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  tags: { type: [String], default: [] },
+  isPinned: { type: Boolean, default: false },
+  userId: { type: String, required: true },
   color: { type: String, default: "#ffffff" },
   fontColor: { type: String, default: "black" },
   fontColorContent: { type: String, default: "black" },
   tagColorContent: { type: String, default: "black" },
-  titleFont: { type: String, default: "Arial" },  // Add default font
-  contentFont: { type: String, default: "Arial" }, // Add default font
-  tagFont: { type: String, default: "Arial" }, // 
-  createdOn: {type: Date, default: new Date().getTime()},
-  modifiedOn: { type: Date, default: Date.now },
-})
+  titleFont: { type: String, default: "Arial" },
+  contentFont: { type: String, default: "Arial" },
+  tagFont: { type: String, default: "Arial" },
+  createdOn: { type: Date, default: Date.now },  // Set to current date on creation
+  modifiedOn: { type: Date }  // Will be updated when modified
+});
+
+// Pre-save hook to update `modifiedOn` field when the document is modified
+userSchema.pre('save', function (next) {
+  if (!this.isNew) {
+    this.modifiedOn = Date.now();  // Set modifiedOn to the current date only on updates
+  }
+  next();
+});
 
 module.exports = mongoose.model("NotesApp", userSchema);
